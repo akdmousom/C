26bc33161b4bbac52e3531a23a0e9f070ea553ae
AKD <contact.mousom@gmail.com>
2024-01-03 01:45:26 +0600

    New branch created for daily coding

---
+++ 2ND_DAY_LEARNING_OF_C/C_New_Lines.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("\nHow can you insert a new line: \n");
+    printf("_____________________________\n\n");
+    printf("This is the second line\n\n");
+    printf("The double back slash N help you to insert blank line \n\n");
+    return 0;
+}
