805708d6f5b77b5baa783f2a4407d25d568bc78f
AKD <contact.mousom@gmail.com>
2024-01-04 22:42:08 +0600

    C Data Types

---
+++ 3RD_DAY_LEARNING_OF_C/1_DATA_TYPE.c
@@ -0,0 +1,22 @@
+//C Data Types
+
+/*
+Data Types
+As explained in the Variables chapter, a variable in C must be a specified data type, and you must use a format specifier inside the printf() function to display it:
+*/
+
+// Create variables
+int myNum = 5;             // Integer (whole number)
+float myFloatNum = 5.99;   // Floating point number
+char myLetter = 'D';       // Character
+
+// Print variables
+printf("%d\n", myNum);
+printf("%f\n", myFloatNum);
+printf("%c\n", myLetter);
+
+/*
+Basic Data Types
+The data type specifies the size and type of information the variable will store.
+
+*/
